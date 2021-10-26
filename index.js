63093f8a7ce40024d875d2f46842a5bc1b37b821
heicg <carlosaraujo@icomp.ufam.edu.br>
2021-10-25 22:48:40 -0400

    Mudanças no layout

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 let selecionar_musica = "Selecione uma música"
 let aguarde = "Aguarde a votação terminar"
 let fim_da_votacao = "Votação Encerrada<br/>A música mais votada é..."
-let tempo_de_cada_votacao = 30
-let quantidade_de_turnos = 3 // isso é definido no banco de dados
+let tempo_de_cada_votacao = 30 // banco de dados
+let quantidade_de_turnos = 3 // banco de dados
+let nome_evento = "nome do evento" // banco de dados
 
+const title = document.querySelector(".title-txt").innerText = nome_evento
+const status = document.querySelector(".info-status-txt").innerHTML = selecionar_musica
+const timer = document.querySelector(".timer-txt").innerText = tempo_de_cada_votacao + "s"
+const turnos = document.querySelector(".turnos-txt").innerText = "1/"+quantidade_de_turnos
+let buttons = document.getElementsByClassName("btn")
 
 function btn_musicas_info () {
-    let musica_selecionada = document.getElementsByClassName
+    //let musica_selecionada = document.getElementsByClassName
     /*
         Ao clicar no icone de info deve ser aberto um popup com as informações da música
     */
@@ -47,4 +53,8 @@ function musica_ganhadora () {
         Deve Atualizar o nome da música [header-winner > span]
         Deve Atualizar o artista [name-winner > span]
     */
+}
+
+function start() {
+
 }
